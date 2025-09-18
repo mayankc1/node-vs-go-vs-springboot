ffba5279d28a48c058329876c6ebbba7115e45c8
mayankc1 <author.mayank.c@gmail.com>
2025-09-18 11:51:13 -0700

    Create UrlShortenerApplication.java

---
+++ SpingBootApp/UrlShortenerApplication.java
@@ -0,0 +1,23 @@
+package com.example.shorten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
+import java.util.concurrent.Executors;
+
+@SpringBootApplication
+public class UrlShortenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortenerApplication.class, args);
+    }
+
+    @Bean
+    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+        return protocolHandler -> {
+            protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+        };
+    }
+}
+
